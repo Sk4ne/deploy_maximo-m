ec57271ed1954e3cc177ba1307f6125a885cfa09
Nlopezp <oldschool##35>
2020-01-11 22:34:28 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app =  express();
+const port = process.env.PORT || 9000;
+const path = require('path');
+
+app.use(express.static(__dirname + '/public'))
+
+app.listen(port,()=>{
+    console.log(`Escuchando peticiones en el puerto${port}`)
+})
---
+++ public/js/chunk-2d210fa0.c708b4f9.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d210fa0"],{b9d7:function(n,u,l){"use strict";l.r(u);var a,c,e=l("2877"),o={},p=Object(e["a"])(o,a,c,!1,null,null,null);u["default"]=p.exports}}]);
+//# sourceMappingURL=chunk-2d210fa0.c708b4f9.js.map
